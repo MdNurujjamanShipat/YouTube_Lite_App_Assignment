7c5295aa0881f1dee569b68443a34e75c33396ce
MdNurujjaman Shipat <mmdshipat100@gmail.com>
2026-03-13 16:10:54 +0600

    youtube lite

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,32 @@
+class AppStrings {
+  AppStrings._();
+  static const String appName = 'YouTube Lite';
+
+  static const String search = 'Search';
+  static const String notifications = 'Notifications';
+
+  static const List<String> categories = [
+    'All',
+    'Trending',
+    'Music',
+    'Gaming',
+    'Live',
+    'News',
+    'Sports',
+    'Learning',
+  ];
+
+  static const String sectionBrowse = 'BROWSE';
+  static const String navHome = 'Home';
+  static const String navShorts = 'Shorts';
+  static const String navSubscriptions = 'Subscriptions';
+  static const String navExplore = 'Explore';
+  static const String navTrending = 'Trending';
+  static const String sectionLibrary = 'LIBRARY';
+  static const String navHistory = 'History';
+  static const String navWatchLater = 'Watch Later';
+  static const String navLikedVideos = 'Liked Videos';
+  static const String sectionMore = 'MORE';
+  static const String navSettings = 'Settings';
+  static const String navHelp = 'Help';
+}
